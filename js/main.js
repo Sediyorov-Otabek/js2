890b723f7023245d0140b8dbf58921a7b1031c6e
Пользователь <otabeksediyorov298@gmail.com>
2024-06-05 16:09:06 +0500

    mana

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// {let birthDate = prompt("Tugilganiz")
+// let age = (2024 - birthDate);
+// alert(age)}
+// { 
+//      prompt("kivadiratning tomonlari berilgan uning peremetiri aniqlansin. are you redy?")
+//      let a= prompt("a sonini kiriting")
+//      let p= alert(  a*4 )
+// }
+// { 
+//      prompt("kivadiratning tomonlari berilgan yuzasi aniqlansin. are you redy?")
+//      let a= prompt("a sonini kiriting")
+//      let s= alert(  a*a )
+// }
+// {
+//      prompt("To'gri tort burchakning tomonlari berilgan uning yuzasi S=a*b, va P=2*(a+b) peremetiri aniqlansin. are you redy?")
+//      let a= prompt("a sonini kiriting")
+//      let b= prompt("b sonini kiriting")
+     
+//      let P= alert (2*(a*b))
+//      let S = alert(a*b)
+// }
+// {
+//      prompt("aylananing diametiri d berilgan uning yuzasi aniqlansin")
+//      let d= prompt("d sonini kiriting")
+//      let L= alert (3.14*d)
+     
+// }
+// {
+//      prompt("kubning yon tomoni a berilgan uning. Uning hajmi (V=a^3) va to'la sirti S=6*a^2 aniqlansin")
+//      let a= prompt(" a sonini kiriting")
+//      let V= alert (a*a*a)
+//      let S= alert (6*a*a)
+     
+// }
+// {
+//      prompt("Paralapepetning tomonlari a b c berilgan. Uning hajmi V=a*b*c va to'la sirti S=2*(a*b+b*c+a*c) aniqlansin")
+//      let a= prompt(" a sonini kiriting")
+//      let b= prompt(" b sonini kiriting")
+//      let c= prompt(" c sonini kiriting")
+//      let V= alert (a*b*c)
+//      let S= alert (2*(a*b+b*c+a*c))
+     
+// }
+// {
+//      prompt("Doiraning uzunligi R berilgan. Uning uzunligi L va S aniqlansin")
+//      let R= prompt(" R sonini kiriting")
+    
+//      let L= alert (2*3.14*R)
+//      let S= alert (3.14*R*R)
+     
+// }
+{
+     prompt("2ta son a va b berilgan ularning o'rta arfmetigi aniqlansin")
+     let a= +prompt(" a sonini kiriting")
+     let b= +prompt(" b sonini kiriting")
+    
+      let j=alert((a+b)/2)
+     
+     
+}
